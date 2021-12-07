e9cb88819ee594544b02f347975f74c86d14b629
pebble78883 <gyojunc@gmail.com>
2021-12-07 22:29:07 +0900

    Init project

---
+++ src/test/java/kr/pebbles/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pebbles.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
